0db76d88bb2dcf695ea3c1b45f1f0bb0c56663e5
Juan Bocanegra <jbocane6@gmail.com>
2022-10-17 13:18:56 -0500

    [UPDATE] Seeding the database with some test data

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,6 +1,19 @@
 import { remultExpress } from 'remult/remult-express'
 import { Task } from 'src/shared/Task';
+import { remult } from 'remult';
 
 export const api = remultExpress({
     entities: [Task]
+    initApi: async () => {
+        const taskRepo = remult.repo(Task);
+        if (await taskRepo.count() === 0) {
+            await taskRepo.insert([
+                { title: "Task a" },
+                { title: "Task b", completed: true },
+                { title: "Task c" },
+                { title: "Task d" },
+                { title: "Task e", completed: true },
+            ]);
+        }
+    }
 });
